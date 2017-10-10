99245ad66d517f2de6363d49701a76e955171820
tejas Pashte <sulkytejas@yahoo.co.in>
2017-10-09 17:16:28 -0700

    router

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -12,123 +12,24 @@ import '../App.css';
 
 class App extends Component {
   state={
-    openModal: false,
     categories:[],
-    body:'',
-    category:'',
-    title:'',
-    author:'',
   }
 
   componentDidMount(){
     getCategories().then(res=> {return this.setState({categories:res.categories})})
-    this.props.itemFetchPost()
   }
 
-  openFormModal = ()=>{
-   this.setState(()=>({openModal: true}));
-  }
-
-  closeFormModal = ()=>{
-   this.setState(()=>({openModal: false}));
-  }
   render() {
-    const {categories,title,body,category,author} = this.state
-    const posts = this.props.posts
+    const {categories} = this.state
     return (
       <div className="App">
-        <div className="container">
-          <div className="navigation">
-            <div className="logo">Readable</div>
-            <button onClick={()=>this.openFormModal()} className="add-post">
-              Add post
-            </button>
-          </div>
-          <Route exact path="/" render={()=>(
-            <Category categories={categories} posts={posts} deletepost={(id)=>(this.props.itemDeletePost(id))}/>
-          )} />
-          {categories.map((category)=>(
-              <Route exact path={category.path} render={()=>(
-                <SingleCategory categories={category} posts={posts}/>
-              )}/>
-          ))}
-
-
-        </div>
-        {/* Modal for Add post */}
-        <Modal
-          isOpen = {this.state.openModal}
-          onRequestClose = {()=>this.closeFormModal()}
-          contentLabel = "Create a Post"
-          className='modal'
-          overlayClassName='overlay'
-          >
-
-          <h1>Create a post</h1>
-          <form>
-            <h2>Title</h2>
-            <input
-              className='form-title'
-              type='text'
-              placeholder='Enter Title'
-              onChange = {(e)=> this.setState({title:e.target.value})}
-              ref={(input) => this.input = input}
-              />
-              <h2>Author</h2>
-              <input
-                className='form-author'
-                type='text'
-                placeholder='Author'
-                onChange = {(e)=> this.setState({author:e.target.value})}
-                ref={(input) => this.input = input}
-              />
-            <h2>Body(Description)</h2>
-            <input
-              className='form-body'
-              type='text'
-              placeholder='Body'
-              onChange = {(e)=> this.setState({body:e.target.value})}
-              ref={(input) => this.input = input}
-              />
-            <h2>category</h2>
-            <select onChange = {(e)=> this.setState({category:e.target.value})}>
-              <option value="none">None</option>
-              {categories.map((category)=> (
-                <option
-                  key={category.name}
-                  value={category.name}>
-                    {category.name}
-                  </option>
-              ))
-            }
-          </select>
-            <button
-              className="close"
-              onClick={()=>this.closeFormModal()}>
-              Close
-            </button>
-            <button
-              className="close"
-              onClick={()=>this.props.itemSendPost(title,body,category,author)}>
-              Submit
-            </button>
-          </form>
-
-        </Modal>
+        <Route exact path="/" component={Category}/>
+        {categories.map((category)=>(
+          <Route exact path={category.path} component={SingleCategory}/>
+        ))}
 
       </div>
     );
   }
 }
-
-const mapStateToProps = ({ postState }) => ({ posts: postState})
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    itemFetchPost: (data) => dispatch(fetchData(data)),
-    itemSendPost: (title,body,category,author) => dispatch(SendPost(title,body,category,author)),
-    itemDeletePost: (id) => dispatch(DeletePost(id))
-  }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default App
--- frontend/src/component/singleCategory.js
+++ frontend/src/component/singleCategory.js
@@ -7,10 +7,7 @@ class SingleCategory extends Component{
       const {categories,posts} = this.props
       return(
       <div>
-        <h1>{categories.name}</h1>
-        <Posts
-          posts={posts.filter((a)=> a.category === categories.name && a.deleted === false)}
-          deletepost={(id)=>(this.props.deletepost(id))}/>
+      tes
       </div>
       )
     }
